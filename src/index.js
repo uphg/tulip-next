f755a76c8b2571586f20b514f68c09078688d834
lvheng <1834805770@qq.com>
2021-06-07 00:34:01 +0800

    让组件支持全局自动导入

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import Button from '../packages/Button.vue'
 import ButtonGroup from '../packages/ButtonGroup.vue'
 import Countdown from '../packages/Countdown.vue'
 import Icon from '../packages/Icon.vue'
+const components = [
+  Button,
+  ButtonGroup,
+  Countdown,
+  Icon
+]
+
+const install = function(Vue, opts = {}) {
+  components.forEach(component => {
+    Vue.component(component.name, component)
+  })
+}
 
 export default {
+  install,
   Button,
   ButtonGroup,
   Countdown,
