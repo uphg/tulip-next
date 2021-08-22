ef6d5866517155203c8446ad1783b4c325e8b02e
lvheng <1834805770@qq.com>
2021-08-22 21:36:55 +0800

    添加一个简单的输入框组件

---
+++ src/lib/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <input
+    type="text"
+    @input="handleInput"
+  >
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    value: String
+  },
+  setup(props, context) {
+    const handleInput = (event: Event) => {
+      const newValue = (event.target as HTMLTextAreaElement).value
+      if (newValue === props.value) return
+      context.emit('update:value', newValue)
+    }
+    return { handleInput }
+  },
+})
+</script>
--- src/views/components/InputDemo.vue
+++ src/views/components/InputDemo.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="input-demo">
-    input 组件示例
+    <h2>input 组件示例</h2>
+    <Input v-model:value="value"></Input>
+    <p>{{ value }}</p>
   </div>
 </template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import Input from '../../lib/Input.vue'
+
+export default defineComponent({
+  components: { Input },
+  setup() {
+    const value = ref(null)
+    
+    return { value }
+  },
+})
+</script>
