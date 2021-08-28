e22535eee5ba115aeeb4c1c923b0f960c768ddd1
lvheng <1834805770@qq.com>
2021-08-29 05:43:18 +0800

    保存库模式配置

---
+++ src/lib/main.ts
@@ -0,0 +1,5 @@
+export { default as Switch } from './Switch.vue'
+export { default as Button } from './Button.vue'
+export { default as Input } from './Input.vue'
+export { default as Dialog } from './Dialog.vue'
+export { useDialog as useDialog } from './useDialog'
