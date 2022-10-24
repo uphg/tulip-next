7e536736c57d86435c58d652d2b56242514b712b
Lvheng <1834805770@qq.com>
2022-10-24 21:48:07 +0800

    docs: 将文档分类

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,26 +7,33 @@ export default defineConfig({
   themeConfig: {
     logo: '/logo.svg',
     nav: [
-      { text: '指南', link: '/guide' },
-      { text: '组件', link: '/components' }
+      { text: '指南', link: '/guide/introduction', activeMatch: '/guide/' },
+      { text: '组件', link: '/components/button', activeMatch: '/components/' }
     ],
-    sidebar: [
-      {
-        text: '指南',
-        items: [
-          { text: 'Tulp UI', link: '/guide/introduction' },
-          { text: '快速开始', link: '/guide/getting-started' },
-        ]
-      },
-      {
-        text: '组件',
-        items: [
-          { text: 'Button 按钮', link: '/components/button' },
-        ]
-      }
-    ]
+    sidebar: {
+      '/guide/': [
+        {
+          text: '介绍',
+          collapsible: true,
+          items: [
+            { text: 'Tulp UI', link: '/guide/introduction' },
+            { text: '快速开始', link: '/guide/getting-started' },
+          ]
+        }
+      ],
+      '/components/': [
+        {
+          text: 'Form 组件',
+          collapsible: true,
+          items: [
+            { text: 'Button 按钮', link: '/components/button' }
+          ]
+        }
+      ]
+    },
   },
   markdown: {
     config: (md) => mdPlugin(md),
   }
 })
+
