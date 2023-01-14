c50c27b3043ee07b13825d77ad39da7a72018542
lvheng <2633565885@qq.com>
2023-01-14 17:12:50 +0800

    feat: 模拟滚动条大小位置移动

--- preview/views/TestScrollbar.vue
+++ preview/views/TestScrollbar.vue
@@ -9,12 +9,5 @@
   width: 800px;
   height: 800px;
   border: 1px solid #0078d7;
-  &::-webkit-scrollbar,
-  &::-webkit-scrollbar-track-piece,
-  &::-webkit-scrollbar-thumb {
-    width: 0;
-    height: 0;
-    display: none;
-  }
 }
 </style>
--- src/components/scrollbar/src/Scrollbar.tsx
+++ src/components/scrollbar/src/Scrollbar.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, type PropType } from 'vue'
+import { computed, defineComponent, ref, type PropType } from 'vue'
 
 const scrollbarProps = {
   directionY: {
@@ -16,16 +16,99 @@ const Scrollbar = defineComponent({
   props: scrollbarProps,
   setup(props, context) {
 
+    const containerRef = ref<HTMLElement | null>(null)
+    const contentRef = ref<HTMLElement | null>(null)
+    const railYRef = ref<HTMLElement | null>(null)
+    const railXRef = ref<HTMLElement | null>(null)
+    const railYBarRef = ref<HTMLElement | null>(null)
+    const railXBarRef = ref<HTMLElement | null>(null)
+
+    const scrollTopRef = ref(0)
+    const scrollLeftRef = ref(0)
+
+    const containerHeight = computed(() => containerRef.value?.offsetHeight || 0)
+    const contentHeight = computed(() => contentRef.value?.offsetHeight || 0)
+    const railScrollTopRef = computed(() => {
+      const railYHeight = railYRef.value?.offsetHeight || 0
+      if (contentHeight.value === null || containerHeight.value === null || railYHeight === null) {
+        return 0
+      } else {
+        const scrollTop = scrollTopRef.value
+        const heightDiff = contentHeight.value - containerHeight.value
+        const barHeight = railYBarRef.value?.offsetHeight || 0
+        // 实际计算：(scrollTop / heightDiff) * (railYHeight - barHeight)
+        return (scrollTop * (railYHeight - barHeight)) / heightDiff
+      }
+    })
+    const railScrollTopPx = computed(() => `${railScrollTopRef.value}px`)
+
+    const yBarSize = computed(() => {
+      const railYHeight = railYRef.value?.offsetHeight || 0
+      if (contentHeight.value === null || containerHeight.value === null || railYHeight === null) {
+        return 0
+      } else {
+        console.log(railYHeight)
+        // 实际计算：(containerHeight.value / contentHeight.value) * railYHeight
+        return Math.min(containerHeight.value, (containerHeight.value * railYHeight) / contentHeight.value)
+      }
+    })
+    const yBarSizePx = computed(() => `${yBarSize.value}px`)
+
+    const handleScroll = (e: Event) => {
+      scrollTopRef.value = containerRef.value.scrollTop
+    }
+
     return () => (
       <div class={['tu-scrollbar', { 'tu-scrollbar--direction-x': props.directionX }]}>
-        <div class={['tu-scrollbar-container', context.attrs.class]}>
-          <div class="tu-scrollbar-content">{context.slots.default?.()}</div>
+        <div ref={containerRef} class={['tu-scrollbar-container', context.attrs.class]} onScroll={handleScroll}>
+          <div ref={contentRef} class="tu-scrollbar-content">{context.slots.default?.()}</div>
         </div>
-        {props.directionY ? <div class="tu-scrollbar-rail tu-scrollbar-rail--vertical"></div> : null }
-        {props.directionX ? <div class="tu-scrollbar-rail tu-scrollbar-rail--horizontal"></div> : null}
+        {props.directionY ? (
+          <div ref={railYRef} class="tu-scrollbar-rail tu-scrollbar-rail--vertical">
+            <div
+              ref={railYBarRef}
+              style={{ top: railScrollTopPx.value, height: yBarSizePx.value }}
+              class="tu-scrollbar-rail__scrollbar"
+            ></div>
+          </div>
+        ) : null }
+        {props.directionX ? (
+          <div ref={railXRef} class="tu-scrollbar-rail tu-scrollbar-rail--horizontal">
+            <div style={{ width: 100 }} class="tu-scrollbar-rail__scrollbar"></div>
+          </div>
+        ) : null}
       </div>
     )
   }
 })
 
 export default Scrollbar
+
+
+// container 100
+
+// content 1000
+
+// rail 30
+
+// raiBar ?
+
+// 第一种：(100 / 1000) * 30
+// 第二种：100 * 30 / 1000
+
+// container content raill railBar
+//         1       1     1       ?
+//         2       2     2
+//         3       3    
+//         4       4    
+//         5       5    
+//                 6    
+//                 7    
+//                 8    
+//                 9    
+//                10    
+//                11    
+//                12    
+//                13    
+//                14    
+//                15    
