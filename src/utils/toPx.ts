11539818c23eb67214bb0e346884174e4191c472
lvheng <2633565885@qq.com>
2023-01-18 14:19:15 +0800

    fix: toPx 函数 parseInt 传值错误

--- src/utils/toPx.ts
+++ src/utils/toPx.ts
@@ -1,3 +1,3 @@
-export function toPx(value: unknown) {
-  return value ? (typeof value === 'string' ? parseInt(value as string, 2) : value) + 'px' : void 0
+export function toPx(value: string | number | undefined | null) {
+  return value ? `${typeof value === 'string' ? parseInt(value, 10) : value}px` : void 0
 }
