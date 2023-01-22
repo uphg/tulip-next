88167499745bedea86430a913a0f66ca9b7f1a85
uphg <1834805770@qq.com>
2023-01-22 18:06:14 +0800

    test: 添加 ImageGroup 预览图片测试

--- preview/views/TestImage2.vue
+++ preview/views/TestImage2.vue
@@ -1,8 +1,15 @@
 <template>
-  <tu-scrollbar trigger="none" style="height: 100vh" >
+  <tu-scrollbar trigger="none" style="height: 100vh"  :on-scroll="onScroll">
     <p v-for="item in 20" :key="item">Hi, I'm Jack{{ item }}</p>
     <tu-image src="https://sponsors.vuejs.org/images/chrome_frameworks_fund.png"/>
     <tu-image width="200px" height="100px" object-fit="contain" src="https://sponsors.vuejs.org/images/chrome_frameworks_fund.png"/>
+    <tu-image-group>
+      <tu-space>
+        <tu-image width="200px" src="/images/1.jpg"/>
+        <tu-image width="200px" src="/images/2.jpg"/>
+        <tu-image width="200px" src="/images/3.jpg"/>
+      </tu-space>
+    </tu-image-group>
   </tu-scrollbar>
 </template>
 
@@ -10,6 +17,10 @@
 import { onMounted, ref } from 'vue'
 
 const divRef = ref(null)
+
+function onScroll() {
+  // console.log('滚动了')
+}
 
 // onMounted(() => {
 //   console.log('divRef.value')
