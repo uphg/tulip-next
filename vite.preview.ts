d5fb86261ae5c69d447c89698870d2118febae42
uphg <1834805770@qq.com>
2023-01-22 18:05:56 +0800

    build: 删除 preview 中的 Windi CSS

--- vite.preview.ts
+++ vite.preview.ts
@@ -2,21 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import WindiCSS from 'vite-plugin-windicss'
-import Restart from 'vite-plugin-restart'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx(),
-    WindiCSS(),
-    Restart({
-      restart: ['../../packages/vite-plugin-windicss/dist/*.js'],
-    }),
+    vueJsx()
   ],
   resolve: {
     alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
       'src': fileURLToPath(new URL('./src', import.meta.url)),
       'docs': fileURLToPath(new URL('./docs', import.meta.url)),
       'preview': fileURLToPath(new URL('./preview', import.meta.url))
