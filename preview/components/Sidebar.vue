c4d6936cfee6156f66ea3163a9183fa61fa7ee66
uphg <1834805770@qq.com>
2023-01-23 15:15:25 +0800

    test: 调整预览页布局，添加示例图片

--- preview/components/Sidebar.vue
+++ preview/components/Sidebar.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="sidebar">
-    
+    <div>
+      <router-link to=""></router-link>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+
+const sidebars = [
+  {
+    title: ''
+  }
+]
+</script>
+
+<style scoped>
+.sidebar {
+  width: 240px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  border-right: 1px solid #eaecef;
+}
+</style>
