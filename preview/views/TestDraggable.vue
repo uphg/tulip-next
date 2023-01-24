aaa1a2ba7bcbff53c4668f8be490b5f834b84dcd
uphg <1834805770@qq.com>
2023-01-24 12:24:52 +0800

    test: 拖拽组件 x, y 轴值响应式

--- preview/views/TestDraggable.vue
+++ preview/views/TestDraggable.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
     <h2>拖拽 div</h2>
-    <tu-draggable>
-      <div class="draggable-demo">
-        我可以拖拽
+    <tu-draggable v-slot="{ x, y }">
+      <div class="draggable-demo1">
+        我可以拖拽 1 号<br><br>x：{{ x }}<br>y：{{ y }}
       </div>
     </tu-draggable>
-    <div ref="draggable" class="draggable-demo" :style="style">
-      我可以拖拽2号，x：{{ x }}，y：{{ y }}
+    <div ref="draggable" class="draggable-demo2" :style="style">
+      我可以拖拽 2 号<br><br>x：{{ x }}<br>y：{{ y }}
     </div>
   </div>
 </template>
@@ -22,13 +22,19 @@ const { x, y, style } = useDraggable(draggable, { initialValue: { x: 100, y: 100
 </script>
 
 <style scoped>
-.draggable-demo {
+.draggable-demo1 {
+  width: 200px;
+  height: 100px;
+  border: 1px solid #9ca3af4d;
+}
+.draggable-demo2 {
   position: fixed;
+  touch-action: none;
   user-select: none;
   cursor: move;
   z-index: 24;
   width: 200px;
-  height: 200px;
+  height: 100px;
   border: 1px solid #9ca3af4d;
 }
 </style>
