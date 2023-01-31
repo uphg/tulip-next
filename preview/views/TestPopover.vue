8f087a393242646857fe93ecbb726465764f166f
lvheng <2633565885@qq.com>
2023-01-31 21:45:15 +0800

    test: 调整 Popover 测试案例

--- preview/views/TestPopover.vue
+++ preview/views/TestPopover.vue
@@ -6,15 +6,21 @@
         <template #trigger>
           <tu-button>悬浮</tu-button>
         </template>
-        <p>{{ content }}</p>
+        {{ content }}
       </tu-popover>
 
-      <tu-popover :content="content" trigger="click">
-        <tu-button>点击</tu-button>
+      <tu-popover trigger="click">
+        <template #trigger>
+          <tu-button>点击</tu-button>
+        </template>
+        {{ content }}
       </tu-popover>
 
-      <tu-popover :content="content" trigger="focus">
-        <tu-button>聚焦</tu-button>
+      <tu-popover trigger="focus">
+        <template #trigger>
+          <tu-button>聚焦</tu-button>
+        </template>
+        {{ content }}
       </tu-popover>
 
       <tu-popover :visible="visible" :content="content" trigger="manual">
@@ -47,59 +53,87 @@
     <div class="popover-demo-1">
       <div style="display:flex; gap: 16px; margin-left: 100px;">
         <tu-popover placement="top-start" :content="content" trigger="click">
-          <tu-button>Top Start</tu-button>
+          <template #trigger>
+            <tu-button>Top Start</tu-button>
+          </template>
         </tu-popover>
         <tu-popover :content="content" trigger="click">
-          <tu-button>Top</tu-button>
+          <template #trigger>
+            <tu-button>Top</tu-button>
+          </template>
         </tu-popover>
         <tu-popover placement="top-end" :content="content" trigger="click">
-          <tu-button>Top End</tu-button>
+          <template #trigger>
+            <tu-button>Top End</tu-button>
+          </template>
         </tu-popover>
       </div>
       <div style="display: flex; width: 300px; padding: 12px 0; justify-content: space-between;">
         <div style="display: flex; flex-direction: column; gap: 40px;">
           <tu-popover placement="left-start" :content="content" trigger="click">
-            <tu-button>Left Start</tu-button>
+            <template #trigger>
+              <tu-button>Left Start</tu-button>
+            </template>
           </tu-popover>
           <tu-popover placement="left" :content="content" trigger="click">
-            <tu-button>Left</tu-button>
+            <template #trigger>
+              <tu-button>Left</tu-button>
+            </template>
           </tu-popover>
           <tu-popover placement="left-end" :content="content" trigger="click">
-            <tu-button>Left End</tu-button>
+            <template #trigger>
+              <tu-button>Left End</tu-button>
+            </template>
           </tu-popover>
         </div>
         <div style="display: flex; flex-direction: column; gap: 40px; margin-left: 340px;">
           <tu-popover placement="right-start" :content="content" trigger="click">
-            <tu-button>Right Start</tu-button>
+            <template #trigger>
+              <tu-button>Right Start</tu-button>
+            </template>
           </tu-popover>
           <tu-popover placement="right" :content="content" trigger="click">
-            <tu-button>Right</tu-button>
+            <template #trigger>
+              <tu-button>Right</tu-button>
+            </template>
           </tu-popover>
           <tu-popover placement="right-end" :content="content" trigger="click">
-            <tu-button>Right End</tu-button>
+            <template #trigger>
+              <tu-button>Right End</tu-button>
+            </template>
           </tu-popover>
         </div>
       </div>
       <div style="display: flex; margin-left: 100px;  gap: 16px;">
         <tu-popover placement="bottom-start" :content="content" trigger="click">
-          <tu-button>Bottom Start</tu-button>
+          <template #trigger>
+            <tu-button>Bottom Start</tu-button>
+          </template>
         </tu-popover>
         <tu-popover placement="bottom" :content="content" trigger="click">
-          <tu-button>Bottom</tu-button>
+          <template #trigger>
+            <tu-button>Bottom</tu-button>
+          </template>
         </tu-popover>
         <tu-popover placement="bottom-end" :content="content" trigger="click">
-          <tu-button>Bottom End</tu-button>
+          <template #trigger>
+            <tu-button>Bottom End</tu-button>
+          </template>
         </tu-popover>
       </div>
     </div>
     <div class="popover-demo">
       <tu-popover :content="content" trigger="click" :style="{ color: 'red' }" class="hi">
-        <tu-button>属性继承</tu-button>
+        <template #trigger>
+          <tu-button>属性继承</tu-button>
+        </template>
       </tu-popover>
     </div>
     <div class="popover-demo">
       <tu-tooltip :content="content" :disabled="disabled" trigger="click">
-        <tu-button>点我</tu-button>
+        <template #trigger>
+          <tu-button>点我</tu-button>
+        </template>
       </tu-tooltip>
       <tu-button @click="disabled = !disabled">切换 disabled</tu-button>        
     </div>
