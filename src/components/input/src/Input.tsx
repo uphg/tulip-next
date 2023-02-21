bc821007e1001f235ff6d93713c5727ddf1ad1f7
lvheng <2633565885@qq.com>
2023-02-21 19:47:05 +0800

    feat: 补充 Input 组件未完成功能

--- src/components/input/src/Input.tsx
+++ src/components/input/src/Input.tsx
@@ -1,5 +1,8 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, shallowRef, Transition } from 'vue'
 import type { PropType } from 'vue'
+import TuBaseIcon from '../../base-icon/src/BaseIcon'
+import { Loading, Clear } from '../../../icons'
+import { emit } from 'process'
 
 const inputProps = {
   value: {
@@ -10,8 +13,19 @@ const inputProps = {
     type: String as PropType<'text' | 'password' | 'textarea'>,
     default: 'text'
   },
+  status: {
+    type: String as PropType<'success' | 'warning' | 'error'>
+  },
+  size: {
+    type: String as PropType<'' | 'large' | 'medium' | 'small'>,
+    validator: (value: string) => {
+      return ['', 'large', 'medium', 'small'].includes(value)
+    }
+  },
   placeholder: String as PropType<string>,
-  disabled: Boolean as PropType<boolean>
+  disabled: Boolean as PropType<boolean>,
+  loading: Boolean as PropType<boolean>,
+  clearable: Boolean as PropType<boolean>
 }
 
 const Input = defineComponent({
@@ -19,50 +33,58 @@ const Input = defineComponent({
   props: inputProps,
   emits: ['update:value'],
   setup(props, context) {
-    const inputRef = ref<HTMLElement | null>(null)
+    const input = shallowRef<HTMLElement | null>(null)
     const isFocus = ref(false)
     const isHover = ref(false)
 
-    const handleInput = (payload: Event) => {
+    function handleInput(payload: Event) {
       const newValue = (payload.target as HTMLTextAreaElement).value
       if (newValue === props.value) return
       context.emit('update:value', newValue)
     }
 
-    const handleBlur = () => {
+    function handleBlur() {
       isFocus.value = false
     }
 
-    const handleMouseDown = (event: Event) => {
+    function handleMouseDown(event: Event) {
       const { tagName } = event.target as HTMLElement
       
       if (tagName !== 'INPUT' && tagName !== 'TEXTAREA') {
         event.preventDefault()
         if (!isFocus.value) {
-          inputRef.value?.focus()
+          input.value?.focus()
         }
       } 
     }
 
-    const handleMouseEnter = () => {
+    function handleMouseEnter() {
       isHover.value = true
     }
 
-    const handleMouseLeave = () => {
+    function handleMouseLeave() {
       isHover.value = false
     }
 
-    const handleFocus = () => {
+    function handleFocus() {
       isFocus.value = true
     }
+
+    function handleClickClear() {
+      context.emit('update:value', '')
+    }
+
     return () => {
-      const { value, type, disabled, placeholder } = props
+      const { value, type, status, disabled, placeholder, loading, clearable, size } = props
       const { slots } = context
+
       return (
         <div
           class={[
             'tu-input',
             {
+              [`tu-input--status-${status}`]: status,
+              [`tu-input--${size}`]: size,
               'tu-input--disabled': disabled,
               'tu-input--focus': isFocus.value,
             }
@@ -82,7 +104,7 @@ const Input = defineComponent({
                 <input
                   class="tu-input__inner"
                   type={type}
-                  ref={inputRef}
+                  ref={input}
                   value={value}
                   disabled={disabled}
                   placeholder={placeholder}
@@ -91,9 +113,28 @@ const Input = defineComponent({
                   onInput={handleInput}
                 />
               </div>
-              {slots.suffix ? (
+              {clearable || slots.suffix || loading ? (
                 <div class="tu-input__suffix">
-                  {slots.suffix()}
+                  {clearable ? (
+                    <div class="tu-input-clear">
+                      <div class="tu-input-clear__inner">
+                        <Transition name="tu-zoom">
+                          {value && (isHover.value || isFocus.value)
+                            ? <TuBaseIcon
+                                class="tu-input-icon"
+                                is={Clear}
+                                onClick={handleClickClear}/>
+                            : null}
+                        </Transition>
+                      </div>
+                    </div>
+                  ) : null}
+                  {slots.suffix?.()}
+                  {loading ? (
+                    <div class="tu-input-loading">
+                      <TuBaseIcon class="tu-input-icon" is={Loading}/>
+                    </div>
+                  ) : null}
                 </div>
               ) : null}
             </div>
