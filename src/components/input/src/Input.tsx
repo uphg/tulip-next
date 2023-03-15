f7b4f9ea39ad726477a25b227ccedcde16ef7df7
uphg <1834805770@qq.com>
2023-03-15 21:20:30 +0800

    feat: Input 组件显示/隐藏密码功能

--- src/components/input/src/Input.tsx
+++ src/components/input/src/Input.tsx
@@ -1,7 +1,10 @@
-import { defineComponent, ref, shallowRef, Transition } from 'vue'
+import { defineComponent, nextTick, ref, shallowRef, Transition, watch, type ExtractPropTypes } from 'vue'
 import type { PropType } from 'vue'
 import TuBaseIcon from '../../base-icon/src/BaseIcon'
-import { Loading, Clear } from '../../../icons'
+import { Loading, Clear, Eye, EyeDisabled } from '../../../icons'
+import { off, on } from '../../../utils'
+
+export type InputProps = ExtractPropTypes<typeof inputProps>
 
 const inputProps = {
   value: {
@@ -24,7 +27,8 @@ const inputProps = {
   placeholder: String as PropType<string>,
   disabled: Boolean as PropType<boolean>,
   loading: Boolean as PropType<boolean>,
-  clearable: Boolean as PropType<boolean>
+  clearable: Boolean as PropType<boolean>,
+  visibilityToggle: String as PropType<'click' | 'mousedown'>,
 }
 
 const Input = defineComponent({
@@ -33,13 +37,19 @@ const Input = defineComponent({
   emits: ['update:value'],
   setup(props, context) {
     const input = shallowRef<HTMLElement | null>(null)
+    const rawValue = ref<InputProps['value']>(null)
     const isFocus = ref(false)
     const isHover = ref(false)
+    const passwordVisible = ref(false)
 
-    function handleInput(payload: Event) {
-      const newValue = (payload.target as HTMLTextAreaElement).value
+    watch(() => props.value, (newValue) => {
+      rawValue.value = newValue
+    })
+
+    function handleInput(e: Event) {
+      const newValue = (e.target as HTMLInputElement).value
       if (newValue === props.value) return
-      context.emit('update:value', newValue)
+      setValue(newValue)
     }
 
     function handleBlur() {
@@ -73,8 +83,37 @@ const Input = defineComponent({
       context.emit('update:value', null)
     }
 
+    function handleClickEye() {
+      if (props.visibilityToggle !== 'click') return
+      setPasswordVisible(!passwordVisible.value)
+    }
+
+    function handleMouseDownEye() {
+      if (props.visibilityToggle !== 'mousedown') return
+      setPasswordVisible(true)
+      on(document, 'mouseup', handleMouseUpDom)
+    }
+
+    function handleMouseUpDom() {
+      setPasswordVisible(false)
+      off(document, 'mouseup', handleMouseUpDom)
+    }
+
+    function setPasswordVisible(value: boolean) {
+      input.value?.blur()
+      passwordVisible.value = value
+      nextTick(() => {
+        input.value?.focus()
+      })
+    }
+
+    function setValue(newValue: InputProps['value']) {
+      rawValue.value = newValue
+      context.emit('update:value', newValue)
+    }
+
     return () => {
-      const { value, type, status, disabled, placeholder, loading, clearable, size } = props
+      const { type, status, disabled, placeholder, loading, visibilityToggle, clearable, size } = props
       const { slots } = context
 
       return (
@@ -102,9 +141,9 @@ const Input = defineComponent({
               <div class="tu-input__inner-wrap">
                 <input
                   class="tu-input__inner"
-                  type={type}
+                  type={passwordVisible.value && type === 'password' ? 'text' : type}
                   ref={input}
-                  value={value}
+                  value={rawValue.value}
                   disabled={disabled}
                   placeholder={placeholder}
                   onBlur={handleBlur}
@@ -118,7 +157,7 @@ const Input = defineComponent({
                     <div class="tu-input-clear">
                       <div class="tu-input-clear__inner">
                         <Transition name="tu-zoom">
-                          {value && (isHover.value || isFocus.value)
+                          {rawValue.value && (isHover.value || isFocus.value)
                             ? <TuBaseIcon
                                 class="tu-input-icon"
                                 is={Clear}
@@ -132,6 +171,15 @@ const Input = defineComponent({
                   {loading ? (
                     <div class="tu-input-loading">
                       <TuBaseIcon class="tu-input-icon" is={Loading}/>
+                    </div>
+                  ) : null}
+                  {visibilityToggle ? (
+                    <div class="tu-input-eye">
+                      <TuBaseIcon
+                        class="tu-input-icon"
+                        onClick={handleClickEye}
+                        onMousedown={handleMouseDownEye}
+                        is={passwordVisible.value ? Eye : EyeDisabled}/>
                     </div>
                   ) : null}
                 </div>
