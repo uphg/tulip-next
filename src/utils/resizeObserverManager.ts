140777b574407c548d03b3ec6c29632018ad6b02
uphg <1834805770@qq.com>
2023-03-18 23:23:12 +0800

    feat: 封装 resizeObserver 全局缓存对象

---
+++ src/utils/resizeObserverManager.ts
@@ -0,0 +1,41 @@
+import type { RawElement } from 'src/types'
+
+type ResizeHandler = (entry: ResizeObserverEntry) => void
+
+function createResizeObserverManager() {
+  const context = {
+    observer: new (window.ResizeObserver)(handleResize),
+    elHandlersMap: new Map()
+  }
+  
+  function handleResize(entries: ResizeObserverEntry[]) {
+    for (const entry of entries) {
+      const handler = context.elHandlersMap.get(entry.target)
+      if (handler !== undefined) {
+        handler(entry)
+      }
+    }
+  }
+  
+  function addHandler<T extends RawElement>(el: T | undefined, handler: ResizeHandler) {
+    if (!el) return
+    context.elHandlersMap.set(el, handler)
+    context.observer.observe(el)
+  }
+  
+  function removeHandler<T extends RawElement>(el: T | undefined) {
+    if (!el || !context.elHandlersMap.has(el)) return
+    context.elHandlersMap.delete(el)
+    context.observer.unobserve(el)
+  }
+
+  return {
+    observer: context.observer,
+    elHandlersMap: context.elHandlersMap,
+    addHandler,
+    removeHandler
+  }
+}
+
+
+export default createResizeObserverManager()
